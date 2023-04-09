7a04b3f6b6ef88f04cd9a0adb4cbefb1427088e3
minhthuy.it <minhthuyxp.it@gmail.com>
2023-04-09 11:53:15 +0700

    commmit code

--- view/saleService/js/main.js
+++ view/saleService/js/main.js
@@ -7,3 +7,36 @@ function onclickRenderIcon(index) {
     ? (btn[index].innerHTML = '<i class="bi bi-dash-square"></i>')
     : (btn[index].innerHTML = '<i class="bi bi-plus-square"></i>');
 }
+
+// let data = [
+//   {
+//     title: "Title câu hỏi thường gặp",
+//     content: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+//   },
+// ];
+
+// data.map((item, index) => {
+//   return (
+//     <div class="col-12">
+//       <div class="collapse_question">
+//         <p>
+//           <span>{item.title}</span>
+//           <button
+//             class="btn-render"
+//             type="button"
+//             data-bs-toggle="collapse"
+//             data-bs-target="#collapseExample"
+//             aria-expanded="false"
+//             aria-controls="collapseExample"
+//             onclick={`onclickRenderIcon(${index})`}
+//           >
+//             <i class="bi bi-plus-square"></i>
+//           </button>
+//         </p>
+//         <div class="collapse" id="collapseExample">
+//           <div class="card card-body card_question">{item.content}</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// });
